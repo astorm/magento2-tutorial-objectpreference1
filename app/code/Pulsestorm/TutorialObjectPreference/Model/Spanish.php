e48476c77fb86ceae975df205755e541c9a2ec1d
Alan Storm <astorm@alanstorm.com>
2015-07-18 14:38:03 -0700

    Adding example of class we'll switch to

---
+++ app/code/Pulsestorm/TutorialObjectPreference/Model/Spanish.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pulsestorm\TutorialObjectPreference\Model;
+class Spanish implements MessageHolderInterface
+{
+    public function getHelloMessage()
+    {
+        return 'Hola';
+    }
+}
