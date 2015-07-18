270ef3286d86ec0a68d6eddab94864dcf93e6623
Alan Storm <astorm@alanstorm.com>
2015-07-18 15:22:04 -0700

    Second messenger

---
+++ app/code/Pulsestorm/TutorialObjectPreference/Model/Messenger2.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pulsestorm\TutorialObjectPreference\Model;
+class Messenger2 extends Messenger
+{    
+    public function getMessage()
+    {
+        return 'Injection?  We don\'t need no stinking injection!';
+    }
+}
