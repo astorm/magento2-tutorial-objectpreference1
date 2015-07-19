fc4071ab901aa750b98918b1547eb2ac18e4fc22
Alan Storm <astorm@alanstorm.com>
2015-07-18 18:23:22 -0700

    One more example

---
+++ app/code/Pulsestorm/TutorialObjectPreference/Model/Messenger3.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pulsestorm\TutorialObjectPreference\Model;
+class Messenger3
+{    
+    public function getMessage()
+    {
+        return 'Injection?  We don\'t need no stinking injection!';
+    }
+}
